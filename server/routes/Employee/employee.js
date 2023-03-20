5c3d2df44531d4c6535185ea84af24f56e134a46
Prathik <prathikk017@gmail.com>
2023-03-20 16:23:31 +0530

    Deleted the unwanted code

--- server/routes/Employee/employee.js
+++ server/routes/Employee/employee.js
@@ -1,20 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const {
-
-  create,
-  find,
-  verifyOTP,
-  login,
-  setPassword,
-} = require('../../controllers/Employee/employee');
-
+const { create } = require('../../controllers/Employee/employee');
 
 router.route('/create').post(create);
-router.route('/find').post(find);
-router.route('/otp').post(verifyOTP);
-router.route('/login').post(login);
-router.route('/setPassword').post(setPassword);
+
 
 module.exports = router;
