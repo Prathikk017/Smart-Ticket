d4e452ca81b11533c26c308c068d8c37b0e1765b
Prathik <prathikk017@gmail.com>
2023-05-22 20:03:46 +0530

    fixed issue when user view page route signin page

--- admin/src/components/Admin Components/Users/ViewUser.jsx
+++ admin/src/components/Admin Components/Users/ViewUser.jsx
@@ -58,7 +58,7 @@ const ViewUser = () => {
 		// Redirect to sign-in page if the user is idle
 		if (isIdle) {
 			window.localStorage.removeItem('Lekpay');
-			history('/');
+			history('/signin');
 		}
 	}, [isIdle, history]);
 
@@ -66,7 +66,7 @@ const ViewUser = () => {
 		const token = window.localStorage.getItem('Lekpay');
 		const Token = JSON.parse(token);
 		if (!Token) {
-			history('/');
+			history('/signin');
 		} else {
 			getSingleUserData();
 		}
