8d6595ee38b7e63fd9cfe6759b88713becd81599
Aakash Hadli <aakashhadli@gmail.com>
2023-06-01 16:44:49 +0530

    fixed single operstor card

--- admin/src/components/About.jsx
+++ admin/src/components/About.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Footer from './Footer';
 import Navbar from './Navbar';
 
 const About = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0); // Scrolls to the top of the page when the component is mounted
+	}, []);
+
 	return (
 		<div>
 			<Navbar />
 			<div className='flex items-center justify-center h-screen'>
 				<div className='mx-auto px-4 max-w-[1024px] text-justify'>
-					<h1 className='font-semibold text-xl'>
+					<h1 className='font-semibold text-2xl'>
 						LekPay is a Global platform for ticketing digitally proudly owned by
 						Powaha Infotech Private Limited.
 					</h1>
 					<p>
 						<br />
 					</p>
-					<p>
+					<p className='text-lg'>
 						LekPay is on a Software as a Service (SaaS) platform where we
 						on-board multiple operators(Transport, Entertainment, Events, etc)
 						and provide them a platform to digitally elevate themselves and
@@ -26,14 +30,14 @@ const About = () => {
 					<p>
 						<br />
 					</p>
-					<p>
+					<p className='text-lg'>
 						The platform provides information of all asset activites and cash
 						collections to the Operators in real time.
 					</p>
 					<p>
 						<br />
 					</p>
-					<p>
+					<p className='text-lg'>
 						We on-board the operator and setup all their assets and related
 						employees on the platform. Commuters/Audience have to install our
 						mobile app <strong>(LEKPAY)</strong> on their mobile phones. <br />{' '}
@@ -43,7 +47,6 @@ const About = () => {
 					</p>
 				</div>
 			</div>
-
 			<div>
 				<Footer />
 			</div>
--- admin/src/components/Admin Components/AdminLayout/IndividualStats/SingleOperCard/IndividualOperatorCard.jsx
+++ admin/src/components/Admin Components/AdminLayout/IndividualStats/SingleOperCard/IndividualOperatorCard.jsx
@@ -1,141 +1,139 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { MdOutlineDirectionsBusFilled } from 'react-icons/md';
-import { BiRupee } from 'react-icons/bi';
-import { IoPeople, IoPieChart } from 'react-icons/io5';
-import { BsFillXDiamondFill } from 'react-icons/bs';
+
+import { IoPeople } from 'react-icons/io5';
 import axios from 'axios';
 import useIdleTimeout from '../../../../../useIdleTimeout';
 const StatsGrid = () => {
-  const history = useNavigate();
-  const ID = window.localStorage.getItem('AdminID');
-  let OperId = JSON.parse(ID);
-  // total operators data
-  const [data, setData] = useState('');
+	const history = useNavigate();
+	const ID = window.localStorage.getItem('AdminID');
+	let OperId = JSON.parse(ID);
+	// total operators data
+	const [data, setData] = useState('');
 
-  const getOperatorsData = async () => {
-    const res = await axios.get('https://amsweets.in/admin/operators');
-    if (res.data.status === 201) {
-      setData(res.data.data);
-    } else {
-      console.log('error');
-    }
-  };
+	const getOperatorsData = async () => {
+		const res = await axios.get('https://lekpay.com/admin/operators');
+		if (res.data.status === 201) {
+			setData(res.data.data);
+		} else {
+			console.log('error');
+		}
+	};
 
-  // const handleClick = () => {
-  // 	history('/admin/operatorsview');
-  // };
+	// const handleClick = () => {
+	// 	history('/admin/operatorsview');
+	// };
 
-  // Call useIdleTimeout and pass in the time to consider the user as idle
-  const isIdle = useIdleTimeout(600000); // set to 10 minute
+	// Call useIdleTimeout and pass in the time to consider the user as idle
+	const isIdle = useIdleTimeout(600000); // set to 10 minute
 
-  //  const verify = async() => {
-  //    const token = window.localStorage.getItem('Lekpay');
-  //    const Token = JSON.parse(token);
-  //    const authorization = `Bearer ${Token}`;
-  //    const res = await axios.post('https://amsweets.in/admin/verify',{
-  // 	 authorization
-  //    });
-  //    if(res.data.status === 201){
-  // 	 console.log(res.data.data);
-  //    }else{
-  // 	 if(res.data.data === 'Token is not valid'){
-  // 	   window.localStorage.removeItem('Lekpay');
-  // 	   history('/');
-  // 	 }
-  //    }
-  //  }
+	//  const verify = async() => {
+	//    const token = window.localStorage.getItem('Lekpay');
+	//    const Token = JSON.parse(token);
+	//    const authorization = `Bearer ${Token}`;
+	//    const res = await axios.post('https://lekpay.com/admin/verify',{
+	// 	 authorization
+	//    });
+	//    if(res.data.status === 201){
+	// 	 console.log(res.data.data);
+	//    }else{
+	// 	 if(res.data.data === 'Token is not valid'){
+	// 	   window.localStorage.removeItem('Lekpay');
+	// 	   history('/');
+	// 	 }
+	//    }
+	//  }
 
-  //  useEffect(() => {
-  //    verify();
-  //    // Run verify() every 10 minute if the user is not idle
-  //    const intervalId = setInterval(() => {
-  // 	 if (!isIdle) {
-  // 	   verify();
-  // 	 }
-  //    }, 600000);
+	//  useEffect(() => {
+	//    verify();
+	//    // Run verify() every 10 minute if the user is not idle
+	//    const intervalId = setInterval(() => {
+	// 	 if (!isIdle) {
+	// 	   verify();
+	// 	 }
+	//    }, 600000);
 
-  //    // Clear the interval when the component unmounts
-  //    return () => clearInterval(intervalId);
-  //  }, [!isIdle]);
+	//    // Clear the interval when the component unmounts
+	//    return () => clearInterval(intervalId);
+	//  }, [!isIdle]);
 
-  useEffect(() => {
-    const logout = async () => {
-      if (isIdle) {
-        window.localStorage.removeItem('Lekpay');
-  
-        try {
-          const res = await axios.patch('https://lekpay.com/admin/logout', {
-            OperId,
-          });
-  
-          if (res.data.status === 201) {
-            console.log('logout');
-          } else {
-            console.log('error');
-          }
-  
-          history('/signin');
-        } catch (error) {
-          console.error(error);
-        }
-      }
-    };
-  
-    logout();
-  }, [isIdle, OperId, history]);
+	useEffect(() => {
+		const logout = async () => {
+			if (isIdle) {
+				window.localStorage.removeItem('Lekpay');
 
-  useEffect(() => {
-    const token = window.localStorage.getItem('Lekpay');
-    const Token = JSON.parse(token);
-    if (!Token) {
-      history('/signin');
-    } else {
-      getOperatorsData();
-    }
-  }, []);
-  return (
-    <div className='grid md:grid-cols-3 gap-4 w-[98%] lg:grid-cols-4 gap-4 w-[98%] mt-4 ml-0 '>
-      {data.length > 0
-        ? data.map((el, i) => {
-            return (
-              <>
-                <BoxWrapper>
-                  <Link to={`/admin/operatorsview/${el.OperId}`}>
-                    <div
-                      className='rounded-full h-12 w-12 flex items-center justify-center bg-pink-400 cursor-pointer'
-                      key={i}
-                    >
-                      <IoPeople
-                        className='text-2xl text-black'
-                        style={{ color: 'white' }}
-                      />
-                    </div>
-                  </Link>
-                  <div className='pl-4 cursor-pointer'>
-                    <div className='flex items-center'>
-                      <Link to={`/admin/operatorsview/${el.OperId}`}>
-                        <strong className='text-xl text-gray-700 font-semibold'>
-                          {el.OperShortName}
-                        </strong>
-                      </Link>
-                    </div>
-                  </div>
-                </BoxWrapper>
-              </>
-            );
-          })
-        : ' '}
-    </div>
-  );
+				try {
+					const res = await axios.patch('https://lekpay.com/admin/logout', {
+						OperId,
+					});
+
+					if (res.data.status === 201) {
+						console.log('logout');
+					} else {
+						console.log('error');
+					}
+
+					history('/signin');
+				} catch (error) {
+					console.error(error);
+				}
+			}
+		};
+
+		logout();
+	}, [isIdle, OperId, history]);
+
+	useEffect(() => {
+		const token = window.localStorage.getItem('Lekpay');
+		const Token = JSON.parse(token);
+		if (!Token) {
+			history('/signin');
+		} else {
+			getOperatorsData();
+		}
+	}, []);
+	return (
+		<div className='grid md:grid-cols-3 gap-4 w-[98%] lg:grid-cols-4 gap-4 w-[98%] mt-4 ml-0 '>
+			{data.length > 0
+				? data.map((el, i) => {
+						return (
+							<>
+								<BoxWrapper>
+									<Link to={`/admin/operatorsview/${el.OperId}`}>
+										<div
+											className='rounded-full h-12 w-12 flex items-center justify-center bg-pink-400 cursor-pointer'
+											key={i}
+										>
+											<IoPeople
+												className='text-2xl text-black'
+												style={{ color: 'white' }}
+											/>
+										</div>
+									</Link>
+									<div className='pl-4 cursor-pointer'>
+										<div className='flex items-center'>
+											<Link to={`/admin/operatorsview/${el.OperId}`}>
+												<strong className='text-xl text-gray-700 font-semibold'>
+													{el.OperShortName}
+												</strong>
+											</Link>
+										</div>
+									</div>
+								</BoxWrapper>
+							</>
+						);
+				  })
+				: ' '}
+		</div>
+	);
 };
 
 export default StatsGrid;
 
 function BoxWrapper({ children }) {
-  return (
-    <div className='bg-white rounded-sm p-4 flex-1 border border-gray-200 flex items-center shadow-md shadow-gray-200'>
-      {children}
-    </div>
-  );
+	return (
+		<div className='bg-white rounded-sm p-4 flex-1 border border-gray-200 flex items-center shadow-md shadow-gray-200'>
+			{children}
+		</div>
+	);
 }
